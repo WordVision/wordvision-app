c09a9686155c804a4dd606a41104c3a736fa8c03
Henrique <henrique.sagara@hotmail.com>
2025-04-03 14:16:33 -0400

    fixing error

--- frontend/lib/supabase.ts
+++ frontend/lib/supabase.ts
@@ -3,10 +3,7 @@ import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =
-  process.env.USE_LOCAL_SUPABASE === "true"
-    ? "http://localhost:54321"
-    : process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 if (!supabaseUrl) throw "missing .env variable: EXPO_PUBLIC_SUPABASE_URL";
 
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
